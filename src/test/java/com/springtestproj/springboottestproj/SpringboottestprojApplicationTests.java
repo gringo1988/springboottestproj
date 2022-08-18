575e11e4991168a52921fc9d78c03820c9cef06b
Gringo <luchavezggringo@gmail.com>
2022-08-18 10:27:00 -0700

    Initial commit

---
+++ src/test/java/com/springtestproj/springboottestproj/SpringboottestprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtestproj.springboottestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottestprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
